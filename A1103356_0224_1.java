148c760268801f1313389316d240c8e983e4ef5e
A1103356 <125680013+tsucheng0718@users.noreply.github.com>
2023-02-26 09:50:47 +0800

    Add files via upload

---
+++ A1103356_0224_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class A1103356_0224_1 {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("請輸入數字:");
+        int num = input.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("這是偶數");
+        }
+        else {
+            System.out.println("這是奇數");
+        }
+     }     
+}
