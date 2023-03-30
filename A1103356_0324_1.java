2fc8235722acf69b9375f60aa751c22de9996334
A1103356 <125680013+tsucheng0718@users.noreply.github.com>
2023-03-30 20:09:46 +0800

    Add files via upload

---
+++ A1103356_0324_1.java
@@ -0,0 +1,72 @@
+import java.util.*;
+class animal {
+    String name;
+    float height;
+    int weight;
+    int speed;
+    void show() {
+        System.out.println("name:" + name + " " + "height:" + height + " " + "weight:" + weight + " " + "speed:" + speed);
+    }
+    double distance(double x) {
+        return x * this.speed;
+    }
+    double distance(int x, double y) {
+        return x * y * this.speed;
+    }
+}
+public class A1103356_0324_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        animal[] animals = new animal[4];
+        animals[0] = new animal();
+        animals[1] = new animal();
+        animals[2] = new animal();
+        animals[3] = new animal();
+
+        animals[0].name = "雪寶";
+        animals[0].height = 1.1f;
+        animals[0].weight = 52;
+        animals[0].speed = 100;
+        animals[0].show();
+        
+        animals[1].name = "驢子";
+        animals[1].height = 1.5f;
+        animals[1].weight = 99;
+        animals[1].speed = 200;
+        animals[1].show();
+
+        animals[2].name = "安那";
+        animals[2].height = 1.7f;
+        animals[2].weight = 48;
+        animals[2].speed = 120;
+        animals[2].show();
+
+        animals[3].name = "愛沙";
+        animals[3].height = 1.7f;
+        animals[3].weight = 50;
+        animals[3].speed = 120;
+        animals[3].show();
+
+        for (int i = 0 ; i < animals.length ; i++) {
+            System.out.println(animals[i].name + "距離計算");
+            System.out.print("enter x(時間):");
+            int num1 = input.nextInt();
+            System.out.print("enter y or not (o/x):");
+            String a = input.next();
+            if (a.equals("o")){
+                System.out.print("enter y(加速度):");
+                double num2 = input.nextFloat();
+                if (num2 == 0) {
+                    System.out.println(animals[i].name + "距離為" + animals[i].distance(num1));
+                } else {
+                    System.out.println(animals[i].name + "距離為" + animals[i].distance(num1, num2));
+                }
+                
+            } else {
+                System.out.println(animals[i].name + "距離為" + animals[i].distance(num1));
+            }
+        }
+
+        input.close();
+    }
+}
