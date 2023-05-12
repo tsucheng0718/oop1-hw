6d2cc96e64e36c28abe500e3f381a26bd3c63029
A1103356 <125680013+tsucheng0718@users.noreply.github.com>
2023-05-12 21:47:44 +0800

    Add files via upload

---
+++ A1103356_0505_1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class A1103356_0505_1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] prize = {1, 3, 5, 7, 9, 11};
+        int[] arr = new int[6];
+
+        for (int i = 0 ; i < 6 ; i++) {
+            System.out.println("enter numbers:");
+            int j = input.nextInt();
+            if (j == prize[i]) {
+                System.out.println("中獎");
+            } else if ((j > 49) || (j < 1)) {
+                throw new IllegalArgumentException("超過範圍");
+                    
+            } else {
+                System.out.println("未中獎");
+            }
+        }
+    }
+}
